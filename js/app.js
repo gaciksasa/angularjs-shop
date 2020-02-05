44f33f813391e146fd6fe37e810e97de735a1200
Sasa.Gacik <sasa.gacik@seavus.com>
2020-02-05 16:54:56 +0100

    single product page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function ($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl: "home.html"
@@ -10,17 +10,38 @@ app.config(function ($routeProvider) {
         .when("/products", {
             templateUrl: "products.html"
         })
+        .when("/products/:productID", {
+            templateUrl: "product-single.html"
+        })
         .when("/contact", {
             templateUrl: "contact.html"
+        })
+        .otherwise({
+            redirectTo: '/'
         });
-});
+}]);
 
 app.controller("productsFeaturedCarousel", function ($scope, $http) {
-    $http.get("js/products.json")
-    .then(function (response) {
+    $http.get("js/products.json").then(function (response) {
         $scope.products = response.data;
-        $scope.isFeatured = function(products) {
+        $scope.isFeatured = function (products) {
             return products.featured == true;
         };
     });
 });
+
+app.controller("productsList", function ($scope, $http) {
+    $http.get("js/products.json").then(function (response) {
+        $scope.products = response.data;
+    });
+});
+
+app.controller("productSingle", ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $scope.id = $routeParams.productID;
+    $http.get("js/products.json").then(function (response) {
+        $scope.products = response.data;
+        $scope.selectedProduct = function (products) {
+            return products.id == $scope.id;
+        };
+    });
+}]);
