ddf0e7ffa6b8f6e606d755fd47f2fe0131a4c984
Sasa.Gacik <sasa.gacik@seavus.com>
2020-02-05 12:33:49 +0100

    fetch featured products from json

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "home.html"
-    })
-    .when("/about", {
-        templateUrl : "about.html"
-    })
-    .when("/products", {
-        templateUrl : "products.html"
-    })
-    .when("/contact", {
-        templateUrl : "contact.html"
+        .when("/", {
+            templateUrl: "home.html"
+        })
+        .when("/about", {
+            templateUrl: "about.html"
+        })
+        .when("/products", {
+            templateUrl: "products.html"
+        })
+        .when("/contact", {
+            templateUrl: "contact.html"
+        });
+});
+
+app.controller("productsFeaturedCarousel", function ($scope, $http) {
+    $http.get("js/productsFeatured.json")
+    .then(function (response) {
+        $scope.products = response.data;
     });
 });
